87a3efa56631203ee796e429ef0ce4568eb2a60e
loudDamage <118292142+loudDamage@users.noreply.github.com>
2023-09-25 16:11:47 -0400

    Delete Exception.java

--- Exception.java
+++
@@ -1,8 +0,0 @@
-public class Exception extends java.lang.Exception {
-    public Exception() {
-        super("My Exception: Invalid input");
-    }
-    public Exception(String message) {
-        super("Special Factorial Exception: " + message);
-    }
-}
